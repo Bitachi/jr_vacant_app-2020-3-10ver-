c09aead306f048db2ca603aa9e776b6462c7ca57
Go Takami <funkykong888@gmail.com>
2020-02-20 15:06:14 +0900

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   get "/about", to: "static_pages#about"
   # For details on the DSL av ailable within this file, see http://guides.rubyonrails.org/routing.html
   get "/signup", to: "users#new"
+  post '/signup',  to: 'users#create'
+  resources :users
 end
