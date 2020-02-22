9b42b0a82b42149b39d7bbf17188f7aa966e3ba2
Go Takami <funkykong888@gmail.com>
2020-02-22 17:39:58 +0900

    gemifle

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,7 +1,22 @@
 class Notification < ApplicationRecord
-  include Encryptor
+  #include Encryptor
   before_save { encrypt_token }
 
+
+  def encrypt(plaintext)
+    key_len = ActiveSupport::MessageEncryptor.key_len
+    secret = Rails.application.key_generator.generate_key('salt', key_len)
+    crypt = ActiveSupport::MessageEncryptor.new(secret)
+    return crypt.encrypt_and_sign(plaintext)
+  end
+
+  def decrypt(ciphertext)
+    key_len = ActiveSupport::MessageEncryptor.key_len
+    secret = Rails.application.key_generator.generate_key('salt', key_len)
+    crypt = ActiveSupport::MessageEncryptor.new(secret)
+    return crypt.decrypt_and_verify(ciphertext)
+  end
+  
   def encrypt_token
      self.token = encrypt(self.token)
   end
