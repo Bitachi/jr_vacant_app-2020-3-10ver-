5103722f2814279cf18d674b87666ceb901760b4
Go Takami <funkykong888@gmail.com>
2020-02-22 17:30:31 +0900

    revised notificationcontroller

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,21 +1,6 @@
 class Notification < ApplicationRecord
-  #include Encryptor
-  #before_save { encrypt_token }
-
-=begin
-  def encrypt(plaintext)
-    key_len = ActiveSupport::MessageEncryptor.key_len
-    secret = Rails.application.key_generator.generate_key('salt', key_len)
-    crypt = ActiveSupport::MessageEncryptor.new(secret)
-    return crypt.encrypt_and_sign(plaintext)
-  end
-
-  def decrypt(ciphertext)
-    key_len = ActiveSupport::MessageEncryptor.key_len
-    secret = Rails.application.key_generator.generate_key('salt', key_len)
-    crypt = ActiveSupport::MessageEncryptor.new(secret)
-    return crypt.decrypt_and_verify(ciphertext)
-  end
+  include Encryptor
+  before_save { encrypt_token }
 
   def encrypt_token
      self.token = encrypt(self.token)
@@ -24,13 +9,11 @@ class Notification < ApplicationRecord
   def get_token
     return decrypt(self.token)
   end
-
-=end
   def self.notify
     notifications = Notification.all
 
     notifications.each do |notification|
-    #  notification.token  = notification.get_token
+      notification.token  = notification.get_token
       system("python3  ./env_py/main.py #{notification.month} #{notification.day} #{notification.hour} #{notification.minute} #{notification.train} #{notification.dep_stn} #{notification.arr_stn} #{notification.token}")
     end
   end
