69941c0ba15e73b6e7e612cb8f2aafca40b46e53
Go Takami <funkykong888@gmail.com>
2020-02-28 17:41:15 +0900

    hgoe

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,6 +2,7 @@ class Notification < ApplicationRecord
   #include Encryptor
   require 'openssl'
   require 'base64'
+  password = ENV['MYAES_KEY']
   # ======================================
 # <暗号化>
 # ======================================
@@ -66,11 +67,11 @@ class Notification < ApplicationRecord
   end
 
   def encrypt_token
-     self.token, self.status = aes_encrypt(self.token, ENV['MYAES_KEY'], 128)
+     self.token, self.salt = aes_encrypt(self.token, password, 128)
   end
 
   def get_token
-    return aes_decrypt(self.token, ENV['MYAES_KEY'], self.salt, 128)
+    return aes_decrypt(self.token, password, self.salt, 128)
   end
 
   def self.notify
--- db/migrate/20200228074910_add_salt_to_notification.rb
+++ db/migrate/20200228074910_add_salt_to_notification.rb
@@ -1,5 +1,5 @@
 class AddSaltToNotification < ActiveRecord::Migration[5.2]
   def change
-    add_column :notifications, :status, :string
+    add_column :notifications, :salt, :string
   end
 end
