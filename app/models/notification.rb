1248cea7d2e87464274ebf71e307fbe3ddbf3f92
Go Takami <funkykong888@gmail.com>
2020-02-28 17:05:58 +0900

    hgoe

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -67,7 +67,7 @@ class Notification < ApplicationRecord
   end
 
   def encrypt_token
-     [self.token, self.salt] = aes_encrypt(self.token, ENV['MYAES_KEY'], 128)
+     self.token, self.salt = aes_encrypt(self.token, ENV['MYAES_KEY'], 128)
   end
 
   def get_token
