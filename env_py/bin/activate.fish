3a4dd3cc9e1b20ae3f6806d809feae9bff0bc23c
Go Takami <funkykong888@gmail.com>
2020-03-01 18:30:41 +0900

    goego

--- env_py/bin/activate.fish
+++ env_py/bin/activate.fish
@@ -52,7 +52,7 @@ if test -z "$VIRTUAL_ENV_DISABLE_PROMPT"
         set -l old_status $status
 
         # Prompt override?
-        if test -n "(env_py) "
+        if test -n "(env_py) "            
             printf "%s%s" "(env_py) " (set_color normal)
         else
             # ...Otherwise, prepend env
